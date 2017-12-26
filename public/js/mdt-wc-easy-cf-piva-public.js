681e3f77a038db8f9136c0e98b45c89ca4e5bf35
madaritech <dm@madaritech.com>
2017-12-26 17:17:16 +0100

    solved bug: checking undefined value for not used tags when user decide to hide select menu.

--- public/js/mdt-wc-easy-cf-piva-public.js
+++ public/js/mdt-wc-easy-cf-piva-public.js
@@ -8,17 +8,20 @@
 
     $('#billing_cfpiva_field').addClass( "validate-required" );
     $('#billing_cfpiva_field').find("label").append( ' <abbr class="required" title="obbligatorio">*</abbr>' );
-    if ($('#billing_ricfatt').val().toUpperCase() == "RICEVUTA") 
-        $('#billing_cfpiva_field').hide();
+    
+    if ($('#billing_ricfatt').val() !== undefined ) {
+        if ($('#billing_ricfatt').val().toUpperCase() == "RICEVUTA") 
+            $('#billing_cfpiva_field').hide();
 
-    $('#billing_ricfatt').live('change', function() {
-                                             if ($('#billing_ricfatt').val().toUpperCase() == 'RICEVUTA') {
-                                                 $('#billing_cfpiva_field').fadeOut();
-                                             }
-                                             else {
-                                                 $('#billing_cfpiva_field').fadeIn();
-                                             }
-                                         });
+        $('#billing_ricfatt').live('change', function() {
+                                                 if ($('#billing_ricfatt').val().toUpperCase() == 'RICEVUTA') {
+                                                     $('#billing_cfpiva_field').fadeOut();
+                                                 }
+                                                 else {
+                                                     $('#billing_cfpiva_field').fadeIn();
+                                                 }
+                                             });
+    }
 
     $('#billing_cfpiva').keyup(function(){
         this.value = this.value.toUpperCase();
